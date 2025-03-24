7efcf9a1c9583346a50ee53d99d254c2a4518af0
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-03-24 10:48:18 +0530

    Update 3169. Count Days Without Meetings.py

--- 3169. Count Days Without Meetings.py
+++ 3169. Count Days Without Meetings.py
@@ -67,4 +67,13 @@ class Solution:
                   return -1
 
 Solution:
-     
+     class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        ans = last = 0
+        for st, ed in meetings:
+            if last < st:
+                ans += st - last - 1
+            last = max(last, ed)
+        ans += days - last
+        return ans
