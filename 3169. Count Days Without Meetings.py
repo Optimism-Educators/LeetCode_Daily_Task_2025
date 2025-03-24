30104c971c2e118c67b9bff7c4a20eb9040958f8
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-03-24 10:47:56 +0530

    Update 3169. Count Days Without Meetings.py

--- 3169. Count Days Without Meetings.py
+++ 3169. Count Days Without Meetings.py
@@ -47,5 +47,24 @@ Constraints:
 1 <= meetings.length <= 105
 meetings[i].length == 2
 1 <= meetings[i][0] <= meetings[i][1] <= days
-""""""
+"""
 
+class Solution:
+    def minAvailableDuration(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        i = 0
+        while i < len(meetings):
+             start, end = meetings[i]
+             j = i + 1
+             while j < len(meetings) and meetings[j][0] <= end:
+                 end = max(end, meetings[j][1])
+                 j += 1
+             if end - start >= days:
+                 return start
+             i = j
+             days -= (end - start)
+             if days <= 0:
+                  return -1
+
+Solution:
+     
