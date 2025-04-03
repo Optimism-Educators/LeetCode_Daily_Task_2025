520403d46daacac699a02953170f0632fbf473ef
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-03 10:11:22 +0530

    Update 2874. Maximum Value of an Ordered Triplet II.py

---
+++ Python_Daily_Solutions/2874. Maximum Value of an Ordered Triplet II/2874. Maximum Value of an Ordered Triplet II.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        ans = mx = mx_diff = 0
+        for x in nums:
+            ans = max(ans, mx_diff * x)
+            mx_diff = max(mx_diff, mx - x)
+            mx = m
